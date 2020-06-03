1c8a27979d9629ca8d490b49288053576437ceea
Wesley Gonçalves Luciano <wesleylg01@gmail.com>
2020-06-03 15:44:54 -0300

    Day two, now with js i can get the states and citys from Brazil using the IBGE API

---
+++ scripts/create-point.js
@@ -0,0 +1,40 @@
+function populateUfFs(){
+    const ufSelect = document.querySelector("select[name=uf]")
+
+    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+    .then( res => res.json() )
+    .then ( states => {
+        for ( const state of states){
+            ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
+        }
+    })
+}
+
+populateUfFs()
+
+
+function getCities (event){
+    const citySelect = document.querySelector("[name=city]")
+    const stateInput = document.querySelector("[name=state]")
+
+    const ufValue = console.log = event.target.value
+    
+    const indexOfSelectState = event.target.selectedIndex
+    
+    stateInput.value = event.target.options[indexOfSelectState].text
+
+    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
+
+    fetch(url)
+    .then( res => res.json() )
+    .then ( cities  => {
+        for ( const city of cities){
+            citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+        }
+
+        citySelect.disabled = false
+    })
+}
+
+document.querySelector("select[name=uf]")
+        .addEventListener("change",getCities)
